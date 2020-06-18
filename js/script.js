78e9791445baf9606c723d3bc7ae62026949f9a2
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2020-06-18 17:10:12 -0300

    Responsive

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// $(".header-banner-images").slick({
+//   dots: true,
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   prevArrow: false,
+//   nextArrow: false,
+// });
+
+$(".best-sellers-carousel").slick({
+  dots: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+});
