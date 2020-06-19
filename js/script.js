8c35319aa9ddfa2b25f6bac8908bb7ee1f05e739
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2020-06-19 19:02:44 -0300

    finish layout flag

--- js/script.js
+++ js/script.js
@@ -7,7 +7,47 @@
 // });
 
 $(".best-sellers-carousel").slick({
+  arrows: false,
   dots: true,
   slidesToShow: 2,
   slidesToScroll: 1,
+  infinite: true,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 899,
+      settings: {
+        slidesToShow: 4,
+        arrows: true,
+        dots: false,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        infinite: true,
+        dots: true,
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 419,
+      settings: {
+        infinite: true,
+        dots: true,
+        centerMode: false,
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 200,
+      settings: {
+        infinite: true,
+        dots: true,
+        centerMode: false,
+        slidesToShow: 2,
+      },
+    },
+  ],
 });
